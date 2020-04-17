b7294e411e11c433f0cbd718cf4c8c9364f273de
Jakobi <Jakobi@192.168.0.2>
2020-04-17 11:27:00 +0200

    Ahoj

---
+++ index.php
@@ -0,0 +1,127 @@
+
+
+
+
+        <?php
+        echo 17;
+echo"<br>";
+print (2.2);
+echo"<br>";
+echo "škola";
+echo"<br>";
+print(FALSE);
+echo " ahoj "." čus ";
+echo "<img src\"koronavirus.jpg.jpg\">";
+echo "\\";
+$a = 9;
+            echo "<br>";
+            $b = 4;
+
+            echo "<br>";
+
+            var_dump ($a + $b);
+
+            echo "<br>";
+
+            $a = 9;
+            echo"<br>";
+            $b = 4;
+
+           var_dump ($a - $b);
+
+            echo "<br>";
+
+            $a = 9;
+            echo"<br>";
+            $b = 4;
+
+            var_dump ($a * $b);
+
+            echo "<br>";
+
+            $a = 9;
+            echo"<br>";
+            $b = 4;
+
+            echo $a / $b;
+
+            echo "<br>";
+
+            $a = 9;
+            echo"<br>";
+            $b = 4;
+
+            var_dump ($a += $b);
+
+            echo "<br>";
+
+            $a = 9;
+            echo"<br>";
+            $b = 4;
+
+            var_dump ($a -= $b);
+
+            echo "<br>";
+
+            $a = 9;
+            echo"<br>";
+            $b = 4;
+
+            var_dump ($a *= $b);
+
+            echo "<br>";
+
+            $a = 9;
+            echo"<br>";
+            $b = 4;
+
+            var_dump ($a /= $b);
+
+            echo "<br>";
+
+            $a = 9;
+            echo"<br>";
+            $b = 4;
+
+            var_dump ($a == $b);
+
+            echo "<br>";
+
+            $a = 9;
+            echo"<br>";
+            $b = 4;
+
+            var_dump ($a != $b);
+
+            echo "<br>";
+
+            $a = 9;
+            echo"<br>";
+            $b = 4;
+
+            var_dump ($a > $b);
+
+            echo "<br>";
+
+            $a = 9;
+            echo"<br>";
+            $b = 4;
+
+            var_dump ($a < $b);
+
+            echo "<br>";
+            $a = 9;
+            echo"<br>";
+            $b = 4;
+
+            var_dump ($a || $b);
+
+            echo "<br>";
+            $a = 9;
+            echo"<br>";
+            $b = 4;
+
+            var_dump ($a & $b);
+
+        ?>
+  
