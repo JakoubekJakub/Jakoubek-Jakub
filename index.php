5be5ae0b30ece149b54118e8dd2f08e3065c7709
Jakobi <Jakobi@192.168.0.2>
2020-04-22 12:24:25 +0200

    Ahoj

--- index.php
+++ index.php
@@ -123,5 +123,63 @@ $a = 9;
 
             var_dump ($a & $b);
 
-        ?>
+            
+        $jakubJakoubek1 = 1;
+        $jakubJakoubek2 = 1;
+        if($jakubJakoubek1 <=  $jakubJakoubek2){
+               echo "Jakub Jakoubek";
+        }
+        if($jakubJakoubek1 > $jakubJakoubek2){
+               echo"Kuba";
+        }
+        else{
+            echo"Jakoubek";
+        }
+        if ($jakubJakoubek1 > $jakubJakoubek2){
+            echo"Kuba";
+            if($jakubJakoubek1 == $jakubJakoubek2){
+                echo"Jakoubek";    
+           }
+        }
+        switch ($jakubJakoubek1) {
+           case 0:
+               break;
+           case 1:
+               echo"ok zoomer";
+           case 2:
+               break;
+           case 3:
+               break;
+           case 4:
+               break;
+           case 5:
+               break;
+           default:
+               break;
+        }                              
+ $produkty =  array(1 =>"op",2 =>"op1", 3 => "op2",4 =>"op3",5 =>"op4",6 =>"op5",7 =>"op6",8 =>"op7",9 =>"op8",10 =>"op9");                  
+ 
+ $produkty1["op"] = array( );
+ $produkty1["up"] = array( );
+ $produkty1["ip"] = array( 
+    array('jméno' => 'Max', 'pohlaví' => 'M', 'druh' => 'pes', 'rasa' => 'knírač', 'věk' => 3 ),
+    array('jméno' => 'Honza', 'pohlaví' => 'M', 'druh' => 'pes', 'rasa' => 'buldok', 'věk' => 6),
+    array('jméno' => 'Vojta', 'pohlaví' => 'M', 'druh' => 'pes', 'rasa' => 'čivava', 'věk' => 4 ),
+    array('jméno' => 'Láďa', 'pohlaví' => 'M', 'druh' => 'pes', 'rasa' => 'německý ovčák', 'věk' => 2),
+    array('jméno' => 'Ivan', 'pohlaví' => 'M', 'druh' => 'pes', 'rasa' => 'labrador', 'věk' => 1 ),
+    array('jméno' => 'Ondra', 'pohlaví' => 'M', 'druh' => 'pes', 'rasa' => 'jezevčík', 'věk' => 7 ),
+    array('jméno' => 'Kuba', 'pohlaví' => 'M', 'druh' => 'pes', 'rasa' => 'bernardýn', 'věk' => 5),
+    array('jméno' => 'Michal', 'pohlaví' => 'M', 'druh' => 'pes', 'rasa' => 'kolie', 'věk' => 9),
+    array('jméno' => 'Jarda', 'pohlaví' => 'M', 'druh' => 'pes', 'rasa' => 'zlatý retrívr', 'věk' => 10),
+    array('jméno' => 'Alex', 'pohlaví' => 'M', 'druh' => 'pes', 'rasa' => 'dalmatýn', 'věk' => 1,5),
+      );
+var_dump($produkty);     
+ var_dump($produkty1); 
+ 
+ 
+         
+             
+ 
+         
+  ?>
   
